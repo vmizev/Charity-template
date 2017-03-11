26050ff05b3cd3f6e6270321c7a078e414885fb4
vmizev <valentin.mizev@gmail.com>
2017-03-11 23:35:44 +0200

    добавлен адаптив

---
+++ app/js/plugin.js
@@ -0,0 +1,12 @@
+;(function ($) {
+	var header = $('.navbar-fixed-top');
+
+	$(window).on('scroll', function () {
+		if( $(window).scrollTop() > 20 ) {
+			header.addClass('on-scroll');
+		} else {
+			header.removeClass('on-scroll');
+		}
+	});
+	
+})(jQuery);
